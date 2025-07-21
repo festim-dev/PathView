a0d1be661cf65faab05410ef541e977223360db0
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2025-07-21 16:41:34 -0400

    added scope and conncetions

--- convert_to_python.py
+++ convert_to_python.py
@@ -1,5 +1,6 @@
 from jinja2 import Environment, FileSystemLoader
 
+
 environment = Environment(loader=FileSystemLoader("templates/"))
 template = environment.get_template("template.py")
 
@@ -10,8 +11,21 @@ import json
 
 data = json.load(open("saved_graphs/test3.json"))
 
+for block in data["nodes"]:
+    block["data"]["label"] = block["data"]["label"].lower().replace(" ", "_")
+
+
+def find_node_by_id(node_id: str) -> dict:
+    for node in data["nodes"]:
+        if node["id"] == node_id:
+            return node
+    return None
+
+
 context = {
     "blocks": data["nodes"],
+    "connections": data["edges"],
+    "find_node_by_id": find_node_by_id,
 }
 
 with open(results_filename, mode="w", encoding="utf-8") as results:
--- templates/template.py
+++ templates/template.py
@@ -17,5 +17,47 @@ class Process(ODE):
         )
 
 {% for block in blocks %}
-{{ block["data"]["label"].lower().replace(" ", "_") }} = Process(name="{{block["data"]["label"].lower().replace(" ", "_")}}")
+{{ block["data"]["label"] }} = Process(
+    name="{{block["data"]["label"]}}",
+    alpha={% if block.data.residence_time != "" %}-1 / float({{block["data"]["residence_time"]}}){% else %}0{% endif %},
+    betas=[],  # TODO implement this
+    gen={% if block.data.source_term != "" %}{{block["data"]["source_term"]}}{% else %}0{% endif %},
+    ic={% if block.data.initial_value != "" %}{{block["data"]["initial_value"]}}{% else %}0{% endif %},
+)
 {% endfor %}
+
+
+Sco = Scope(
+    labels=[
+        {% for block in blocks %}"{{ block["data"]["label"] }}",{% endfor %}
+    ]
+)
+
+blocks = [
+    {% for block in blocks %}{{ block["data"]["label"] }},{% endfor %}
+]
+
+
+connections = [
+    {% for connection in connections %}Connection({{ find_node_by_id(connection["source"])["data"]["label"] }}, {{ find_node_by_id(connection["target"])["data"]["label"] }}),
+    {% endfor %}{% for block in blocks %}Connection({{ block["data"]["label"] }}, Sco[{{ loop.index0 }}]),
+    {% endfor -%}
+]
+
+Sim = Simulation(
+    blocks,
+    connections,
+    log=True,
+    Solver=RKF21,
+    tolerance_lte_rel=1e-4,
+    tolerance_lte_abs=1e-9,
+)
+
+if __name__ == "__main__":
+    Sim.run(10)
+
+    Sim.save("model.mdl")
+
+    fig, ax = Sco.plot()
+
+    plt.show()
