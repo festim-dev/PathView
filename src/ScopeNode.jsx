17e2a25de7e69e144378d84515da0bc0476b10ee
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2025-07-25 19:59:07 -0400

    improved style of ScopeNode

--- src/ScopeNode.jsx
+++ src/ScopeNode.jsx
@@ -5,8 +5,8 @@ export default function ScopeNode({ data }) {
   return (
     <div
       style={{
-        width: 180,
-        height: 100,
+        width: 100,
+        height: 120,
         background: '#DDE6ED',
         color: 'black',
         borderRadius: 12,
@@ -16,7 +16,31 @@ export default function ScopeNode({ data }) {
         cursor: 'pointer',
       }}
     >
-      <div style={{ marginBottom: 4 }}>{data.label}</div>
+      {/* Small window/screen area */}
+      <div 
+        style={{
+          width: '70%',
+          height: '50%',
+          background: '#f0f0f0',
+          border: '2px solid #333',
+          borderRadius: 4,
+          margin: '0 auto 8px auto',
+          position: 'relative',
+        }}
+      >
+        {/* Inner screen area */}
+        <div 
+          style={{
+            width: '80%',
+            height: '70%',
+            background: 'white',
+            border: '1px solid #999',
+            margin: '4px auto',
+            borderRadius: 2,
+          }}
+        />
+      </div>
+      <div style={{ marginBottom: 4, textAlign: 'center' }}>{data.label}</div>
 
       <Handle type="target" position="left" style={{ background: '#555' }} />
     </div>
