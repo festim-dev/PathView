0340a960b057e68ee76434c9b6ac4ed6aa84cef7
Yucky <trashyiiiiiii@gmail.com>
2025-10-26 17:09:23 -0400

    default node refactor

--- src/components/nodes/DefaultNode.jsx
+++ src/components/nodes/DefaultNode.jsx
@@ -4,21 +4,15 @@ import { Handle } from '@xyflow/react';
 export default function DefaultNode({ data }) {
   return (
     <div
+      class="node"
       style={{
-        width: 180,
-        background: data.nodeColor || '#DDE6ED',
-        color: 'black',
-        borderRadius: 8,
-        padding: 10,
-        fontWeight: 'bold',
-        position: 'relative',
-        cursor: 'pointer',
+        background: data.nodeColor,
       }}
     >
       <div style={{ marginBottom: 4 }}>{data.label}</div>
 
-      <Handle type="target" position="left" style={{ background: '#555' }} />
-      <Handle type="source" position="right" style={{ background: '#555' }} />
+      <Handle type="target" position="left" style={{ background: "#555" }} />
+      <Handle type="source" position="right" style={{ background: "#555" }} />
     </div>
   );
 }
