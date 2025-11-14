4b4f9479939cd901c2c53faff5dd163353dc2473
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2025-11-14 11:56:06 -0500

    fixed plugin

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
+    react(),
     tailwindcss(),
   ],
   resolve: {
